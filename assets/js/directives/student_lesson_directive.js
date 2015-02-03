cc0bc71d63a7f743a66c697cdf2ccef710d56265
Kenny Czadzeck <kennycgit@gmail.com>
2015-02-03 15:22:13 -0800

    Change query to Module, and remove lesson links from being dependent on .current.params

--- assets/js/directives/student_lesson_directive.js
+++ assets/js/directives/student_lesson_directive.js
@@ -2,11 +2,9 @@ app.directive("studentLessonDirective", ["$routeParams", "Lesson", function($rou
   return {
     templateUrl: "/js/templates/student_lesson_template.html",
     link: function(scope, elem, attr) {
-      // query database for lesson based on route params
       Lesson.get({id: $routeParams.lessonId}, function(data) {
         var content = marked(data.body);
         angular.element.find("#student-lesson-display")[0].innerHTML = content;
-        console.log(data, content);
       });
     }
   };
--- assets/js/directives/student_sidebar_directive.js
+++ assets/js/directives/student_sidebar_directive.js
@@ -1,9 +1,10 @@
-app.directive("studentSidebarDirective", ["$route", "Library", function($route, Library) {
+app.directive("studentSidebarDirective", ["$route", "Module", function($route, Module) {
   return {
     templateUrl: "/js/templates/student_sidebar_template.html",
     link: function(scope, elem, attr) {
-      Library.get({id: $route.current.params.libraryId}, function(data) {
-        scope.library = data.modules;
+      scope.params = $route.current.params;
+      Module.query({library: $route.current.params.libraryId}, function(modules) {
+        scope.library = modules;
       });
     }
   };
